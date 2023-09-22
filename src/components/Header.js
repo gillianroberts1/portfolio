4fa44c39bde28dba42e1ee13708509fc7c914332
gillian <gillianroberts@me.com>
2023-09-22 18:30:02 +0100

    styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,30 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-
-
+import styled from "styled-components";
 
 const Header = () => {
-    return ( 
+  return (
+    <>
+      <>
+        <h1>My Portfolio</h1>
+      </>
 
-        <>
+      <nav>
         <ul>
-        <li>
-        <Link to="/aboutme">About Me</Link>
-        </li>
-        
-        <li>
-        <Link to="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-        <Link to="/contact">Contact Me</Link>
-        </li>
+          <li>
+            <Link to="/aboutme">About Me</Link>
+          </li>
+
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Me</Link>
+          </li>
         </ul>
+      </nav>
+    </>
+  );
+};
 
-        </>
-
-
-        
-     );
-}
- 
 export default Header;
