d862897e26791205f3a13ad857b7a8b4660ab444
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2023-11-24 18:33:06 +0000

    more projects added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,7 +1,15 @@
 import { Col } from "react-bootstrap";
 import "./ProjectCard.css";
 
-const ProjectCard = ({ title, description, stack, imgUrl, link, viewApp }) => {
+const ProjectCard = ({
+  title,
+  description,
+  features,
+  stack,
+  imgUrl,
+  link,
+  viewApp,
+}) => {
   return (
     <Col sm={6} md={4}>
       <div className="proj-imgbx">
@@ -11,6 +19,8 @@ const ProjectCard = ({ title, description, stack, imgUrl, link, viewApp }) => {
           <span>{description}</span>
           <br></br>
           <span>{stack}</span>
+          <br></br>
+          <span>{features}</span>
           <br></br>
           <a className="play-app" href={link} target="_blank" rel="noreferrer">
             {viewApp}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,12 +3,16 @@ import projectImg1 from "../assets/img/SGARhome.png";
 import projectImg2 from "../assets/img/BrewDogHome.png";
 import ProjectCard from "./ProjectCard";
 import projectImg3 from "../assets/img/ServivorsHome.png";
+import projectImg4 from "../assets/img/WiggleWaggyWalks.png";
+import projectImg5 from "../assets/img/LouLousLibrary.png";
+import projectImg6 from "../assets/img/tennis_order.png";
+
 import colorSharp from "../assets/img/color-sharp.png";
 
 const Projects = () => {
   const projects = [
     {
-      title: "Recipes Website",
+      title: "Sizzle & Grill Aromatic Recipes",
       description: "Group Project- October 23",
       stack: "React, MongoDB, Express, Node.js, 1st party API, Responsive ",
       imgUrl: projectImg1,
@@ -24,11 +28,33 @@ const Projects = () => {
       viewApp: "PLAY APP",
     },
     {
-      title: "Servivors Tennis Club Booking System",
+      title: "Wiggle Waggy Walks",
+      description: "Group Project - November 23",
+      features: "Authentication, Image Upload, Real-time Chat",
+      stack: "Java, Spring, Firebase, React",
+      imgUrl: projectImg4,
+    },
+    {
+      title: "SERVivors Tennis Club Booking System",
       description: "Solo Project - August 23",
       stack: "Python, PostgreSQL, Flask",
       imgUrl: projectImg3,
     },
+    
+    {
+      title: "Lou Lou's Library",
+      description: "Solo Project - July 23",
+      stack: "Python, Flask",
+      imgUrl: projectImg5,
+    },
+    {
+      title: "Pink Floyd Tennis Shop",
+      description: "Solo Project - July 23",
+      stack: "Python, Flask",
+      imgUrl: projectImg6,
+    },
+    
+
   ];
 
   return (
